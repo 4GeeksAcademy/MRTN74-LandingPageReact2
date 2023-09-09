98f8ec935bec977a0b78ab288c53bf7c98515684
Martin Gallo <136949107+MRTN74@users.noreply.github.com>
2023-09-08 21:39:50 -0500

    Add files via upload

---
+++ src/js/component/home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {Navbar} from "./navbar.jsx"
+import {Jumbotron} from "./jumbotron.jsx"
+import {Cards} from "./cards.jsx"  
+import  "/workspaces/MRTN74-landingPageReact/src/styles/index.css"
+import Footer from "./Footer.jsx";
+import "../../styles/index.css"
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Home = () => {
+	return (
+		<div className="pb-5 position-relative"style={{height:"100vh",}}>
+			<Navbar/>
+			<Jumbotron/>
+
+			<div className="container" style={{display:"flex", justifyContent: "space-around"}}>
+				<Cards/> 
+				<Cards/> 
+				<Cards/> 
+				<Cards/>
+			</div>
+			<Footer/>
+		</div>
+	);
+};
+
+export default Home;
